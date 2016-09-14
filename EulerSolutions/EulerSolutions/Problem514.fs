f2277066382adaf80432d22b52130ebcb08e9e94
jcook <vaderboy@gmail.com>
2016-09-14 09:54:19 -0500

    [EULER-1] Change power operator that handle integer powers of floats from ^ to **. This matches the other F# power operators.
    Clean up unit tests so that asserts look more like C# function calls. This is just to make things look more comparable to C# tests.

--- EulerSolutions/EulerSolutions/Problem514.fs
+++ EulerSolutions/EulerSolutions/Problem514.fs
@@ -1,5 +1,5 @@
 ﻿module Problem514
-open System
+open System.Collections.Generic
 open MathMethods
 open Geometry
 
@@ -8,6 +8,42 @@ let shapeProbability geoboardOrder fixedHoleCount vertexCount =
     let holeProbability = (float geoboardOrder)/(geoboardOrder + 1 |> float)
 
     (pinProbability**vertexCount)*(holeProbability**fixedHoleCount)
+
+//let unpinVertex (vertices : List<Point>) (unpinNumber : int) =
+//    if unpinNumber < 0 || unpinNumber >= vertices.Count then
+//        failwithf "Cannot remove vertex at %d from a list with length %d" unpinNumber vertices.Count
+//
+//    let point1 = 
+//        match unpinNumber with
+//        | 0 -> vertices.[vertices.Count - 1]
+//        | _ -> vertices.[unpinNumber - 1]
+//
+//    let point2 = vertices.[unpinNumber]
+//
+//    let point3 =
+//        match unpinNumber with
+//        | _ when unpinNumber = (vertices.Count - 1) -> vertices.[0]
+//        | _ -> vertices.[unpinNumber + 1]
+//
+//    vertices
+
+let unpinVertex (vertices : Point list) (unpinNumber : int) =
+    if unpinNumber < 0 || unpinNumber >= vertices.Length then
+        failwithf "Cannot remove vertex at %d from a list with length %d" unpinNumber vertices.Length
+
+    let point1 = 
+        match unpinNumber with
+        | 0 -> vertices.[vertices.Length - 1]
+        | _ -> vertices.[unpinNumber - 1]
+
+    let point2 = vertices.[unpinNumber]
+
+    let point3 =
+        match unpinNumber with
+        | _ when unpinNumber = (vertices.Length - 1) -> vertices.[0]
+        | _ -> vertices.[unpinNumber + 1]
+
+    vertices
 
 type ShrinkingBoard (geoboardOrder : int) =
     let vertices = [
